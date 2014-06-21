35b096bc079c4ef72e92d1398cee1deb986879a7
Brian Zambrano <brianz@gmail.com>
2014-06-21 13:55:30 -0700

    Install phidgets drivers and python library
    Seems to be working.

--- roles/phidgets/files/install_phidgets.sh
+++ roles/phidgets/files/install_phidgets.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+DOWNLOAD_DIR=/home/brianz
 LIB_NAME=libphidget.tar.gz
-LIB_VERSION=libphidget_2.1.8.20140319.tar.gz
+LIB_VERSION=libphidget-2.1.8.20140319
 
-curl http://www.phidgets.com/downloads/libraries/$LIB_NAME -o /tmp/$LIB_NAME
+if [ ! -f $DOWNLOAD_DIR/$LIB_NAME ]; then
+    curl -L http://www.phidgets.com/downloads/libraries/$LIB_NAME -o $DOWNLOAD_DIR/$LIB_NAME
+fi
 
-tar zxvf /tmp/$LIB_VERSION
-cd /tmp/$LIB_VERSION
+tar zxf $DOWNLOAD_DIR/$LIB_NAME
+cd $DOWNLOAD_DIR/$LIB_VERSION
 ./configure
 make
 make install
